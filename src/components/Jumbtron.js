2220ce128114da7c39c6c93acf2af875d3ac9e24
shubham <sbangade1995@gmail.com>
2020-11-16 19:20:00 -0800

    shopping list project

---
+++ src/components/Jumbtron.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Jumbtron = () => (
+    <div className="jumbotron jumbotron-fluid bg-dark text-white">
+      <div className="container text-sm-center pt-5">
+        <h1 className="display-2">Shubham's Shopping List</h1>
+        <p className="lead">Shopping Made Easy</p>
+      </div>
+    </div>
+);
